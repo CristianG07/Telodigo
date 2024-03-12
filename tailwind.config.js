e4353f8a0b060827646c62381464d7a63dd334c2
Cristian Gonzalez <crisgonzalez07g@gmail.com>
2024-03-11 21:53:47 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,11 @@ export default {
         'secondary': '#A238FF'
       },
       backgroundImage: {
-        'hero': 'url("./src/assets/img/hero_bg.jpg")',
-        'footer': 'url("./src/assets/img/footer_bg.jpg")',
-        'left_bg': 'url("./src/assets/img/left_bg.png")',
-        'darck_bg': 'url("./src/assets/img/darck_bg.png")',
-        'center_gradian': 'url("./src/assets/img/center_gradian.png")'
+        'hero': "url('/src/assets/img/hero_bg.jpg')",
+        'footer': 'url("/src/assets/img/footer_bg.jpg")',
+        'left_bg': 'url("/src/assets/img/left_bg.png")',
+        'darck_bg': 'url("/src/assets/img/darck_bg.png")',
+        'center_gradian': 'url("/src/assets/img/center_gradian.png")'
       }
     },
   },
